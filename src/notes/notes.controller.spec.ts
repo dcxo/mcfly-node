b62ec2ff41fa9352cf03d373ebae07326ed1be8d
David Castilla Ortiz <im@dcxo.dev>
2021-06-17 12:44:18 +0200

    Added tests

--- src/notes/notes.controller.spec.ts
+++ src/notes/notes.controller.spec.ts
@@ -1,18 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NotesController } from './notes.controller';
+import { NotesService } from './notes.service';
 
 describe('NotesController', () => {
-  let controller: NotesController;
+    let controller: NotesController;
+    let noteService: NotesService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [NotesController],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [NotesController],
+            providers: [NotesService]
+        }).compile();
 
-    controller = module.get<NotesController>(NotesController);
-  });
+        controller = module.get<NotesController>(NotesController);
+        noteService = module.get<NotesService>(NotesService)
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(controller).toBeDefined();
+    });
+
+    it(`findAll`, async () => {
+        const result = [{ title: 'hola', content: 'mundo', isFavourite: true }]
+        jest.spyOn(noteService, 'getAll').mockImplementation(() => result);
+
+        expect(await controller.findAll()).toBe(result);
+    });
+
 });
--- src/notes/notes.service.spec.ts
+++ src/notes/notes.service.spec.ts
@@ -2,17 +2,33 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { NotesService } from './notes.service';
 
 describe('NotesService', () => {
-  let service: NotesService;
+    let service: NotesService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [NotesService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [NotesService],
+        }).compile();
 
-    service = module.get<NotesService>(NotesService);
-  });
+        service = module.get<NotesService>(NotesService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
+
+    it('should get an empty array', () => {
+        expect(service.getAll()).toStrictEqual([]);
+    })
+
+    it('should add a note', () => {
+        service.create({ title: 'hola', content: 'mundo' });
+        expect(service.getAll()).toStrictEqual([{ title: 'hola', content: 'mundo', isFavourite: false }])
+    })
+
+    it('should first note be favourite', () => {
+        service.create({ title: 'hola', content: 'mundo' });
+        service.changeFavourite(0);
+        expect(service.getById(0)).toMatchObject({ isFavourite: true })
+    })
+
 });
